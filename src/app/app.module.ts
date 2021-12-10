e96105c4004e6e1155f458bfe2994785d9c4505a
Сергей Еременко <sadgramazeka@gmail.com>
2021-12-10 09:37:56 +0200

    Add components, routes and design registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { HomeComponent } from './presentation/components/home/home.component';
+import { LoginComponent } from './presentation/components/login/login.component';
+import { RegistrationComponent } from './presentation/components/registration/registration.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
+    
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
