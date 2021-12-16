89ad798b7bf7c899d1d8d16668b66024f769c300
AndriyKovalets <andriykovalec@gmail.com>
2021-12-17 00:06:31 +0200

    Fix addworkspacetUrl.

--- src/app/configs/api-endpoints.ts
+++ src/app/configs/api-endpoints.ts
@@ -4,6 +4,6 @@ export const baseUrl = environment.apiUrl + '/api';
 
 export const registerUrl = baseUrl + '/Authentication/registration';
 export const loginUrl = baseUrl + '/Authentication/login';
-export const addworkspacetUrl = baseUrl + 'Workspace/addworkspace';
+export const addworkspacetUrl = baseUrl + '/Workspace/addworkspace';
 export const refreshTokenUrl = baseUrl + '/Authentication/refreshToken';
 export const logoutUrl = baseUrl + '/Authentication/logout';
