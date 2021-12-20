56bf52411b2bb365e491d5659cf32f5b96700a6a
Сергей Еременко <sadgramazeka@gmail.com>
2021-12-20 14:41:20 +0200

    bug fix with dropdown menu

---
+++ src/app/presentation/components/modal-invites/modal-invites.component.ts
@@ -0,0 +1,20 @@
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-invites',
+  templateUrl: './modal-invites.component.html',
+  styleUrls: ['./modal-invites.component.css']
+})
+export class ModalInvitesComponent implements OnInit {
+
+  userId: string;
+  constructor(private authenticationService: AuthenticationService) { }
+
+
+  ngOnInit() {
+    if (this.authenticationService.currentUser.Id)
+      this.userId = this.authenticationService.currentUser.Id;
+  }
+
+}
