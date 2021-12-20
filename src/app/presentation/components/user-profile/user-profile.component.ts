fde47a1038a7b0bbe4a054a5d5b1db209e2161d3
AndriyKovalets <andriykovalec@gmail.com>
2021-12-20 15:26:18 +0200

    remove concole log form onInit method

--- src/app/presentation/components/user-profile/user-profile.component.ts
+++ src/app/presentation/components/user-profile/user-profile.component.ts
@@ -25,7 +25,6 @@ export class UserProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log(this.router.url);
     this.userService.getUserProfile().subscribe((data: UserProfile) =>{
       this.userProfile = data;
       this.userProfileForm.patchValue(this.userProfile);
