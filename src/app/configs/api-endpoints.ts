1edf942c53f38018641ea90b157388491a088fc4
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-12-20 20:46:48 +0200

    add getUserWorkspaceList api endpoint

--- src/app/configs/api-endpoints.ts
+++ src/app/configs/api-endpoints.ts
@@ -8,6 +8,7 @@ export const refreshTokenUrl = baseUrl + '/Authentication/refreshToken';
 export const logoutUrl = baseUrl + '/Authentication/logout';
 
 export const addworkspacetUrl = baseUrl + '/Workspace/addworkspace';
+export const getUserWorkspaceList = baseUrl + '/Workspace/getworlspacelist';
 
 export const userProfileUrl = baseUrl + '/User/getpersonalinfo';
 
