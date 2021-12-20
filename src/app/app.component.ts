9c69ab0787f1e86c999ff4de52b00b8658b0954e
AndriyKovalets <andriykovalec@gmail.com>
2021-12-20 15:35:48 +0200

    remove unused imports.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthenticationService } from './core/services/authentication.service';
 
 
 @Component({
