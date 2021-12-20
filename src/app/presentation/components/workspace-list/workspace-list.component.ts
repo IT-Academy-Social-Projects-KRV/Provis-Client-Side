4a192c0c92327663c29856cd2dccf87cfbb360d6
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-12-20 21:06:09 +0200

    add subscribe on get user workspace method

--- src/app/presentation/components/workspace-list/workspace-list.component.ts
+++ src/app/presentation/components/workspace-list/workspace-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkspaceService } from 'src/app/core/services/workspace.service';
+import { UserWorkspace } from 'src/app/core/models/userWorkspaceList';
+
 
 @Component({
   selector: 'app-workspace-list',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workspace-list.component.css']
 })
 export class WorkspaceListComponent implements OnInit {
+  userWorkspaceList: UserWorkspace[];
+  constructor(private workspaceService: WorkspaceService) {}
 
-  constructor() { }
-  
   ngOnInit() {
+    this.workspaceService.getUserWorkspace().subscribe((data: UserWorkspace[]) => {
+      this.userWorkspaceList = data;
+      console.log(data);
+      
+    });
   }
 
 }
