5d0ca31e8b9718471b43454397fa05e5be7e1835
Сергей Еременко <sadgramazeka@gmail.com>
2021-12-29 16:57:55 +0200

    add left menu and add routes with components for this menu

---
+++ src/app/presentation/components/member-calendar/member-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-calendar',
+  templateUrl: './member-calendar.component.html',
+  styleUrls: ['./member-calendar.component.css']
+})
+export class MemberCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
