08c77c37fb967c156fd506bca1b61bb9ab156253
MishA <mishaknyazzzz@gmail.com>
2022-01-03 19:39:28 +0200

    changes

--- src/app/core/services/workspace.service.ts
+++ src/app/core/services/workspace.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateWorkspace } from '../models/workspace';
-import { addworkspacetUrl, getUserWorkspaceList, inviteUser } from 'src/app/configs/api-endpoints';
+import { addworkspacetUrl, deleteUserFromWorkspaseUrl, getUserWorkspaceList, getWorkspaceUsersUrl, inviteUser } from 'src/app/configs/api-endpoints';
 import { Observable } from 'rxjs';
 import { UserWorkspace } from '../models/userWorkspaceList';
 import { UserInvite } from '../models/userInvite';
+import { WorkspaceMembers } from '../models/workspaceUsersList';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,8 @@ export class WorkspaceService {
     private readonly getUserWorkspaceList = getUserWorkspaceList;
     private readonly WorkspaceListUrl = addworkspacetUrl;
     private readonly getUserInvite = inviteUser;
+    private readonly getWorkspaceUsers = getWorkspaceUsersUrl;
+    private readonly delUserWorksp = deleteUserFromWorkspaseUrl;
 
     private httpOption = {
         headers: new HttpHeaders({
@@ -35,5 +38,13 @@ export class WorkspaceService {
 
     public InviteUser(invite: UserInvite): Observable<void> {
         return this.http.post<void>(this.getUserInvite, invite, this.httpOption);
+    }
+
+    public getWorkspaceUserList(workspaceId: number): Observable<WorkspaceMembers[]>{
+        return this.http.get<WorkspaceMembers[]>(this.getWorkspaceUsers+"/"+workspaceId+"/members", this.httpOption);
+    }
+
+    public delUserFromWorksp(workspaceId: number, userId: string): Observable<void>{
+        return this.http.delete<void>(this.delUserWorksp+"/"+workspaceId+"/user/"+userId, this.httpOption);
     }
 }
--- src/app/presentation/components/member-managment/member-managment.component.ts
+++ src/app/presentation/components/member-managment/member-managment.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { numbers } from '@material/slider';
+import { WorkspaceMembers } from 'src/app/core/models/workspaceUsersList';
+import { WorkspaceService } from 'src/app/core/services/workspace.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-member-managment',
@@ -8,8 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberManagmentComponent implements OnInit {
 
-  constructor() { }
+  workspaceId: number;
+  workspaceUserList: WorkspaceMembers[];
+  constructor(private workspServise: WorkspaceService, private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.workspaceId = Number(this.route.snapshot.paramMap.get('id'))
+    this.workspServise.getWorkspaceUserList(this.workspaceId).subscribe(data=>{
+      this.workspaceUserList=data;})
+      
+  })}
 
+  delete(userId:string): void{
+    this.workspServise.delUserFromWorksp(this.workspaceId, userId).subscribe(
+      () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: "User was successfully deleted"
+        })
+        // window.location.reload();
+      }
+    )
+  }
 }
