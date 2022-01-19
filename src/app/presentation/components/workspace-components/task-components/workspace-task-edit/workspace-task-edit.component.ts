5cbf0ccc132beba85dccc9042b630b9912e6145b
Maxim <62253266+FeloniukM@users.noreply.github.com>
2022-01-19 17:01:44 +0200

    fix

--- src/app/presentation/components/workspace-components/task-components/workspace-task-edit/workspace-task-edit.component.ts
+++ src/app/presentation/components/workspace-components/task-components/workspace-task-edit/workspace-task-edit.component.ts
@@ -8,7 +8,6 @@ import { TaskWorkerRole } from 'src/app/core/models/task/taskWorkerRoles';
 import { WorkspaceMembers } from 'src/app/core/models/workspace/workspaceMembers';
 import { AlertService } from 'src/app/core/services/alerts.service';
 import { TaskService } from 'src/app/core/services/task.service';
-import { WorkspaceService } from 'src/app/core/services/workspace.service';
 
 @Component({
   selector: 'app-workspace-task-edit',
@@ -32,10 +31,8 @@ export class WorkspaceTaskEditComponent implements OnInit {
   id: string;
   demoForm: FormGroup;
 
-  constructor(private workspaceService: WorkspaceService,
-    private forbBuilder: FormBuilder,
+  constructor(private forbBuilder: FormBuilder,
     private alertService: AlertService,
-    private workspaceServ: WorkspaceService,
     public dialog: MatDialog,
     private taskServise: TaskService) {
     this.detalInfoForm = forbBuilder.group({
@@ -50,10 +47,6 @@ export class WorkspaceTaskEditComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.workspaceService.getWorkspaceUserList(this.workspaceId).subscribe((data: WorkspaceMembers[]) => {
-      this.workspaceMemberList = data;
-    });
-
     this.taskServise.getStatusTask().subscribe((statList: TaskStatus[]) => {
       this.statusList = statList;
     });
@@ -62,7 +55,7 @@ export class WorkspaceTaskEditComponent implements OnInit {
       this.taskRole = role;
     });
 
-    this.taskServise.getTaskInfo(this.taskId).subscribe((data: TaskDetalInfo) => {
+    this.taskServise.getTaskInfo(this.workspaceId, this.taskId).subscribe((data: TaskDetalInfo) => {
       this.detalInfoForm = this.forbBuilder.group({
         "Name": data.name,
         "Description": data.description,
@@ -75,9 +68,9 @@ export class WorkspaceTaskEditComponent implements OnInit {
     });
 
   }
+
   EditTask() {
     if (this.detalInfoForm.valid) {
-
       this.taskChangeInfo = this.detalInfoForm.value;
       this.taskChangeInfo.id = this.taskId;
       this.taskChangeInfo.workspaceId = this.workspaceId;
